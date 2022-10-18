04084cf94fee7fbc4420368bbad9570cb6d1e5e1
Kyle King <KyleKing@users.noreply.github.com>
2022-10-18 19:29:36 -0400

    refactor: report markdown and filter by min_score

--- pattern_feedback_tool/linters.py
+++ pattern_feedback_tool/linters.py
@@ -1,27 +1,59 @@
 """Linting Feedback Tools."""
 
+from collections.abc import Callable
 from pathlib import Path
+from typing import Literal, TypeAlias
 
+from beartype import beartype
+from beartype.typing import Protocol
 from radon.cli import Config
 from radon.cli.harvest import CCHarvester
-from radon.complexity import SCORE
+from radon.complexity import LINES
+
+# FIXME: Aggregate flake8, mypy, radon, and other warnings?
+
+# ---------------- Radon ----------------
 
 
-def run_radon(arg_path: Path) -> dict:
+class _HarvesterProtocol(Protocol):
+    def as_md(self) -> str:
+        ...
+
+
+_HARV_TYPE: TypeAlias = Callable[[list[str], Config], _HarvesterProtocol]
+
+
+@beartype
+def run_radon(
+    arg_path: Path,
+    *,
+    min_score: Literal['A', 'B', 'C', 'D', 'E', 'F'] = 'A',
+    radon_harvester: _HARV_TYPE | None = None,
+) -> str:
     """Run radon to check cyclomatic complexity.
+
+    - Based on: https://github.com/rubik/xenon/blob/b63601f6a0aba48150d92913c3522e19e33870f8/xenon/core.py
+    - Radon Documentation: https://radon.readthedocs.io/en/latest/intro.html
+    - Radon Source Code: https://github.com/rubik/radon/blob/941f8e20bdd8672d39fc395ebe893e43c1619cdf/radon/cli/__init__.py
 
     Args:
         arg_path: "Directory containing source files to analyze, or multiple file paths"
+        min_score: minimum score to log
+
+    Returns:
+        str: markdown-formatted table
 
     """
+    _harvester: _HARV_TYPE = radon_harvester or CCHarvester
     config = Config(
         exclude='',
         ignore='',
-        order=SCORE,
+        order=LINES,
         no_assert=False,
         show_closures=False,
-        min='A',
+        min=min_score,
         max='F',
+        show_complexity=True,
     )
-    harv = CCHarvester([arg_path.as_posix()], config)
-    return harv._to_dicts()
+    harv = _harvester([arg_path.as_posix()], config)
+    return harv.as_md()
--- tests/test_linters.py
+++ tests/test_linters.py
@@ -1,3 +1,5 @@
+from radon.cli.harvest import CCHarvester
+
 from pattern_feedback_tool.linters import run_radon
 
 from .configuration import TEST_DATA_DIR
@@ -6,6 +8,6 @@ SAMPLE_CODE_DIR = TEST_DATA_DIR / 'sample_code'
 
 
 def test_run_radon(assert_against_cache):
-    results = run_radon(SAMPLE_CODE_DIR)
+    results = run_radon(SAMPLE_CODE_DIR, min_score='D', radon_harvester=CCHarvester)
 
-    assert_against_cache(results)
+    assert_against_cache(results.split('\n'))
