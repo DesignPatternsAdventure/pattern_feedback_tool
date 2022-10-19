a971886b66e189c36dce20b654697bb1fe6597be
Kyle King <KyleKing@users.noreply.github.com>
2022-10-18 21:25:02 -0400

    feat: add a sort-of-working pycg

--- pattern_feedback_tool/graphics.py
+++ pattern_feedback_tool/graphics.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 from beartype import beartype
 from code2flow.engine import code2flow
+from pycg import formats
+from pycg.pycg import CallGraphGenerator
+from pycg.utils.constants import CALL_GRAPH_OP
 
 # ---------------- code2flow ----------------
 
@@ -28,4 +31,36 @@ def run_code2flow(
 # ---------------- TODO: pylint.pyreverse ----------------
 # See: https://github.com/PyCQA/pylint/blob/c2989ad5c71b3e1be0f0a7e5297f9b7e47fa2766/tests/pyreverse/conftest.py
 
-# ---------------- TODO: pycg ----------------
+# ---------------- pycg ----------------
+
+
+@beartype
+def run_pycg(
+    arg_path: Path,
+    package: str | None = None,
+    max_iter: int = -1,
+    **kwargs,
+) -> dict:
+    """Run pycg to generate a call graph.
+
+    - Based on: https://github.com/vitsalis/PyCG/blob/99c991e585615263f36fae5849df9c2daa684021/pycg/__main__.py#L75-L89
+
+    Args:
+        arg_path: "Directory containing source files to analyze, or multiple file paths"
+
+    """
+    cg = CallGraphGenerator([arg_path], package=package, max_iter=max_iter, operation=CALL_GRAPH_OP, **kwargs)
+    cg.analyze()
+
+    formatter = formats.Simple(cg)
+
+    # FASTEN format is probably easier to parse, but Simple is more human readable
+    # formatter = formats.Fasten(cg, package=package, product="", forge="", version="", timestamp=0)
+
+    return formatter.generate()
+
+    # Also experimented with the graph output, but similar to Simple
+    # as_formatter = formats.AsGraph(cg)
+    # output = as_formatter.generate()
+
+    # TODO: Still investigating how to best utilize these call graphs: https://github.com/vitsalis/PyCG#examples
--- tests/test_graphics.py
+++ tests/test_graphics.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-from pattern_feedback_tool.graphics import run_code2flow
+from pattern_feedback_tool.graphics import run_code2flow, run_pycg
 
 from .configuration import SAMPLE_CODE_DIR
 
@@ -14,3 +14,10 @@ def test_run_code2flow(assert_against_cache, fix_test_cache):
         soup = BeautifulSoup(fp)
     text_tags = sorted(_t.text for _t in soup.find_all('text'))
     assert_against_cache(text_tags)
+
+
+def test_run_pycg(assert_against_cache):
+    result = run_pycg(SAMPLE_CODE_DIR / 'game_view.py', package='rpg')
+
+    # TODO: This is just a deterministic way to log the call graph. Seems to change when re-run
+    assert_against_cache(sorted(result.keys()))
