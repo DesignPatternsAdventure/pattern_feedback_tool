7ff2f52b327193423ffd3993ea6570a88302f6d0
Kyle King <KyleKing@users.noreply.github.com>
2022-10-26 17:32:40 -0400

    feat: initialize a table output

--- pattern_feedback_tool/lint_parsers.py
+++ pattern_feedback_tool/lint_parsers.py
@@ -1,35 +1,46 @@
 """Parsers for lint module output."""
 
 import json
+from enum import Enum
 
 import pyparsing as pp
 from beartype import beartype
+from calcipy.doit_tasks.base import defaultdict
 from pydantic import BaseModel
 from rich.console import Console
+from rich.table import Table
+
+
+class Linter(Enum):
+
+    PYLINT = 'pylint'
+    FLAKE8 = 'flake8'
 
 
 class LintLog(BaseModel):
 
+    source: Linter
+    message: str
+    message_id: str
     kind: str | None = None
-    obj: str | None = None
+    file_path: str
     line: str
     column: str
     end_line: str | None = None
     end_column: str | None = None
-    file_path: str
-    message: str
-    message_id: str
+    obj: str | None = None
 
     @classmethod
     def from_pylint(cls, pylint_data: dict) -> 'LintLog':
         """Dropped keys: module, symbol."""
         keys = ['obj', 'line', 'column', 'message']
         return cls(
+            source=Linter.PYLINT,
+            message_id=pylint_data['message-id'],
             kind=pylint_data['type'],
+            file_path=pylint_data['path'],
             end_line=pylint_data['endLine'],
             end_column=pylint_data['endColumn'],
-            file_path=pylint_data['path'],
-            message_id=pylint_data['message-id'],
             **{key: pylint_data[key] for key in keys},
         )
 
@@ -52,7 +63,7 @@ def _extract_flake8_data(line: str) -> LintLog:
     flake8_grammar = full_path + code + obj + message
     parsed_log = flake8_grammar.parse_string(line, parse_all=True)
 
-    return LintLog(**parsed_log.as_dict())
+    return LintLog(source=Linter.FLAKE8, **parsed_log.as_dict())
 
 
 @beartype
@@ -68,4 +79,27 @@ def parse_flake8_logs(flake8_logs: str) -> list[LintLog]:
 @beartype
 def display_lint_logs(console: Console, logs: list[LintLog]) -> None:
     """Use rich to display an easily readable output from found LintLogs."""
-    raise NotImplementedError('...')
+    table = Table(show_header=True, header_style='bold')
+
+    table.add_column('Source', style='Dim')
+    table.add_column('File Path')
+    table.add_column('Code', style='Bold')
+    table.add_column('Message')
+
+    grouped_logs = defaultdict(list)
+    for log in logs:
+        grouped_logs[log.file_path].append(log)
+
+    # TODO: Style the message_ids based on severity!
+    for pth in sorted(grouped_logs):  # FIXME: This is sort-able inline!
+        for log in grouped_logs[pth]:
+            # FIXME: Get the full path
+            link = f'{log.file_path}:{log.line}:{log.column}'
+            table.add_row(
+                log.source.value,
+                f'[link={link}]{link}[/]',
+                log.message_id,
+                f'{log.message} ({log.obj})' if log.obj else log.message,
+            )
+
+    console.print(table)
--- tests/test_lint_parsers.py
+++ tests/test_lint_parsers.py
@@ -4,57 +4,58 @@ from rich.console import Console
 
 from pattern_feedback_tool.lint_parsers import display_lint_logs, parse_flake8_logs, parse_pylint_json_logs
 
+PYLINT_LOGS = json.dumps([
+    {
+        'type': 'refactor',
+        'module': 'src.__main__',
+        'obj': 'MyWindow',
+        'line': 13,
+        'column': 0,
+        'endLine': 13,
+        'endColumn': 14,
+        'path': 'src/__main__.py',
+        'symbol': 'too-many-ancestors',
+        'message': 'Too many ancestors (6/3)',
+        'message-id': 'R0901',
+    },
+    {
+        'type': 'warning',
+        'module': 'src.tasks.task1',
+        'obj': 'SpriteGenerator.__init__',
+        'line': 35,
+        'column': 8,
+        'endLine': 35,
+        'endColumn': 12,
+        'path': 'src/tasks/task1.py',
+        'symbol': 'unnecessary-pass',
+        'message': 'Unnecessary pass statement',
+        'message-id': 'W0107',
+    },
+])
+
+FLAKE8_LOGS = """
+src/tasks/__init__.py:3:1: F401 '.task1.SpriteGenerator' imported but unused
+src/tasks/task1.py:61:1: E800 Found commented out code
+"""
+
 
 def test_parse_pylint_json_logs(assert_against_cache):
-    pylint_logs = json.dumps([
-        {
-            'type': 'refactor',
-            'module': 'src.__main__',
-            'obj': 'MyWindow',
-            'line': 13,
-            'column': 0,
-            'endLine': 13,
-            'endColumn': 14,
-            'path': 'src/__main__.py',
-            'symbol': 'too-many-ancestors',
-            'message': 'Too many ancestors (6/3)',
-            'message-id': 'R0901',
-        },
-        {
-            'type': 'warning',
-            'module': 'src.tasks.task1',
-            'obj': 'SpriteGenerator.__init__',
-            'line': 35,
-            'column': 8,
-            'endLine': 35,
-            'endColumn': 12,
-            'path': 'src/tasks/task1.py',
-            'symbol': 'unnecessary-pass',
-            'message': 'Unnecessary pass statement',
-            'message-id': 'W0107',
-        },
-    ])
-
-    results = parse_pylint_json_logs(pylint_logs)
+    results = parse_pylint_json_logs(PYLINT_LOGS)
 
     assert_against_cache([_r.dict() for _r in results])
 
 
 def test_parse_flake8_logs(assert_against_cache):
-    flake8_logs = """
-src/tasks/__init__.py:3:1: F401 '.task1.SpriteGenerator' imported but unused
-src/tasks/task1.py:61:1: E800 Found commented out code
-"""
-
-    results = parse_flake8_logs(flake8_logs)
+    results = parse_flake8_logs(FLAKE8_LOGS)
 
     assert_against_cache([_r.dict() for _r in results])
 
 
 def test_display_lint_logs():
     console = Console()
-    logs = []
+    logs = parse_pylint_json_logs(PYLINT_LOGS) + parse_flake8_logs(FLAKE8_LOGS)
 
     display_lint_logs(console, logs)
 
-    assert False
+    # FIXME: Stub Console to write a proper test. See link for examples
+    #  https://github.com/Textualize/rich/tree/4001aa91790c5faf46d9fbca35d335d76de52376/tests
