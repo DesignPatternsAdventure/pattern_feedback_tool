b82e7afb0dfd2f4ecb1f60ca50e1673cc75e15cd
Kyle King <KyleKing@users.noreply.github.com>
2022-11-10 20:52:28 -0500

    bump: version 0.2.3 → 0.3.0

--- pattern_feedback_tool/__init__.py
+++ pattern_feedback_tool/__init__.py
@@ -4,7 +4,7 @@ from warnings import filterwarnings
 
 from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
 
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 __pkg_name__ = 'pattern_feedback_tool'
 
 # FYI: https://github.com/beartype/beartype#are-we-on-the-worst-timeline
